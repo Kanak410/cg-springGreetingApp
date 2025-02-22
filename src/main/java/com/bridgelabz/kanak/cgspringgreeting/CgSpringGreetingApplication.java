1d6412e45cf520a27b7e8dca17e979ba342909cc
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-22 15:59:02 +0530

    [kanak]initial commit

---
+++ src/main/java/com/bridgelabz/kanak/cgspringgreeting/CgSpringGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.kanak.cgspringgreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgSpringGreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgSpringGreetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/kanak/cgspringgreeting/CgSpringGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.kanak.cgspringgreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgSpringGreetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
