3337061f81a19bce2cb54e3d2d49f56eae80261d
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-24 22:07:43 +0530

    [kanak] Add. Implemented saveGreeting API with JPA

--- src/main/java/com/bridgelabz/kanak/cgspringgreeting/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/kanak/cgspringgreeting/controller/GreetingController.java
@@ -1,29 +1,38 @@
 package com.bridgelabz.kanak.cgspringgreeting.controller;
 
 import com.bridgelabz.kanak.cgspringgreeting.dto.Greeting;
+import com.bridgelabz.kanak.cgspringgreeting.repository.GreetingRepository;
 import com.bridgelabz.kanak.cgspringgreeting.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-    private final GreetingService greetingService;
+//    private final GreetingService greetingService;
+//
+//    public GreetingController(GreetingService greetingService) {
+//        this.greetingService = greetingService;
+//    }
+//
+//
+//    @GetMapping
+//    public Greeting greeting(
+//            @RequestParam(value = "firstName", required = false) String firstName,
+//            @RequestParam(value = "lastName", required = false) String lastName){
+//        return greetingService.createGreeting(firstName,lastName);
+//    }
+//
+//    @PostMapping
+//    public Greeting createGreeting(@RequestBody Greeting greeting){
+//        return greetingService.createGreeting(greeting.getFirstName(),greeting.getLastName());
+//    }
+    @Autowired
+    private GreetingRepository greetingRepository;
 
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
-
-    @GetMapping
-    public Greeting greeting(
-            @RequestParam(value = "firstName", required = false) String firstName,
-            @RequestParam(value = "lastName", required = false) String lastName){
-        return greetingService.createGreeting(firstName,lastName);
-    }
-
-    @PostMapping
-    public Greeting createGreeting(@RequestBody Greeting greeting){
-        return greetingService.createGreeting(greeting.getFirstName(),greeting.getLastName());
+    @PostMapping()
+    public Greeting saveGreeting(@RequestBody Greeting greeting){
+        return greetingRepository.save(greeting);
     }
 
 
--- src/main/java/com/bridgelabz/kanak/cgspringgreeting/dto/Greeting.java
+++ src/main/java/com/bridgelabz/kanak/cgspringgreeting/dto/Greeting.java
@@ -1,26 +1,66 @@
 package com.bridgelabz.kanak.cgspringgreeting.dto;
 
+//public class Greeting {
+//    private final String message;
+//    private final String firstName;
+//    private final String lastName;
+//
+//    public Greeting(String message, String firstName, String lastName) {
+//        this.message = message;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//}
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Greeting {
-    private final String message;
-    private final String firstName;
-    private final String lastName;
-
-    public Greeting(String message, String firstName, String lastName) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+    public Greeting(){}
+    public Greeting(String message) {
         this.message = message;
-        this.firstName = firstName;
-        this.lastName = lastName;
+}
 
-    }
+public Greeting(String message, String firstName, String lastName) {
 
-    public String getFirstName() {
-        return firstName;
-    }
+}
 
-    public String getLastName() {
-        return lastName;
-    }
+public long getId() {
 
-    public String getMessage() {
-        return message;
-    }
+    return id;
+}
+
+public void setId(long id) {
+    this.id = id;
+}
+
+public String getMessage() {
+    return message;
+}
+
+public void setMessage(String message) {
+    this.message = message;
+}
+
+
 }
